ac0d02085f761911504405166944bf83fae39eff
Sergio Santos <sergiossantos0@gmail.com>
2021-07-17 20:11:34 -0300

    commit de criacao do projeto nodejs

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(3000, () => console.log('servidor rodando lindo na porta 3000'))
+
+app.get('/atendimentos', (req, res) =>res.send('Você esta na rota de atendimentos e esta realizando um GET!!'))
