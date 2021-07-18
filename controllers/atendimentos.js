fdd29a5c8da1d8e5ec2473bdb3d4425844cb8ddb
Sergio Santos <sergiossantos0@gmail.com>
2021-07-18 10:28:30 -0300

    configuração de infraestrutura

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,11 @@
+const Atendimento = require('../models/atendimentos')
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você esta na rota de atendimentos e esta realizando um GET!!!'))
 
     app.post('/atendimentos', (req, res) =>{
-        console.log(req.body)
-        res.send('Você esta na rota de atendimentos e esta realizando um post!!!')
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento)
+        console.log('dados salvos no banco')
+
     })
 }
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const customExpress = require("./config/customExpress")
+const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
-const app = customExpress()
-
-app.listen(3000, () => console.log('servidor rodando lindo na porta 3000'))
+conexao.connect(erro =>{
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log('conectado com sucesso!!')
+        Tabelas.init(conexao)
+        const app = customExpress()
+        app.listen(3000, () => console.log('servidor rodando lindo na porta 3000'))
+    }
+})
